c1377facdf958bd89521712375b722a0cc0e1df0
Karl Glaser <kg@papercloud.com.au>
2014-11-04 18:05:27 +1100

    working

--- src/run.rb
+++ src/run.rb
@@ -2,38 +2,39 @@ require_relative 'input_parser'
 require_relative 'torrent_file_saver'
 require 'pry'
 
+def mark_line_as_done(line)
+  text = File.read(ARGV[0])
+  replace = text.gsub(line, "-" + line[1..line.length])
+  File.open(ARGV[0], "w") {|file| file.puts replace}
+end
+
+
 parser = InputParser.new(ARGV[0])
 saver = TorrentFileSaver.new(parser)
 saver.clear_torrents
 
-f = File.open(ARGV[0], "r+")
-old_pos = 0
-f.each_line do |line|
+lines = File.readlines(ARGV[0])
+lines.each do |line|
   if line[0] == "+"
-    f.pos = old_pos   # this is the 'rewind'
     new_song_name = line.split("+")[1].split("\r")[0]
     song = Song.new(new_song_name)
-    # parser.songs << song
+    parser.songs << song
 
-    # parser.search_next_song(song)
+    parser.search_next_song(song)
 
-    # parser.search_google_on_missing(song)
-
-    old_pos = f.pos
-    f.print "-"
+    parser.search_google_on_missing(song)
 
     if (song.sorted_torrents.first)
-      # saver.save_to_file(song)
+      saver.save_to_file(song)
       to_run = "java -cp ./bin Torrenado -f #{song.saved_file} -s \"#{song.name}\" -u larry -p [randomlygeneratedpassword]"
       puts "Running #{to_run}"
       system to_run
-
-      # f.print "-"
-      # f.pos = f.pos - 1
+      mark_line_as_done(line)
     end
   end
+
 end
 
-f.close
+
 
 
